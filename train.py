5481f227d53f84ba0abcbb33319fe64d1b95f787
Siriwut <67328603+Younive@users.noreply.github.com>
2026-03-05 17:41:07 +0700

    minor change: import DataModule correctly

--- train.py
+++ train.py
@@ -3,10 +3,10 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from data import CoLADataModule
+from data import DataModule
 from model import ColaModel
 
-cola_data = CoLADataModule()
+cola_data = DataModule()
 cola_model = ColaModel()
 
 def main():
