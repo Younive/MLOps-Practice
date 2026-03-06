c885c9e7286e9ad6cc23fc0c08ea335869c7b3ed
Siriwut <67328603+Younive@users.noreply.github.com>
2026-03-06 14:43:24 +0700

    add wandb logger

--- train.py
+++ train.py
@@ -1,16 +1,18 @@
 import torch
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from data import DataModule
 from model import ColaModel
 
+
 cola_data = DataModule()
 cola_model = ColaModel()
+wandb_logger = WandbLogger(project="cola_model", name="cola_run_1")
 
 def main():
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    checkpoint_callback = ModelCheckpoint(
     dirpath="./models",
     monitor="val_acc",
     mode="min",
@@ -26,7 +28,7 @@ def main():
         gpus = (1 if torch.cuda.is_available() else 0),
         max_epochs = 3,
         fast_dev_run = True,
-        logger = TensorBoardLogger('logs/', name='cola_model', version=1),
+        logger = wandb_logger,
         callbacks = [checkpoint_callback, early_stopping_callback]
     )
     trainer.fit(cola_model, cola_data)
