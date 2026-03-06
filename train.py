16051b8b61e033715f99c38fda23be96e423469b
Siriwut <67328603+Younive@users.noreply.github.com>
2026-03-06 14:56:45 +0700

    added SimpleVisualizationLogger callback to the trainer, which logs a batch of validation data to wandb at the end of each validation epoch. Also added pandas import for data manipulation in the callback.

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import torch
+import wandb
+import pandas as pd
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
@@ -6,16 +8,39 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from data import DataModule
 from model import ColaModel
 
-
 cola_data = DataModule()
 cola_model = ColaModel()
-wandb_logger = WandbLogger(project="cola_model", name="cola_run_1")
+
+class SimpleVisualizationLogger(pl.Callback):
+    def __init__(self, datamodule):
+        super().__init__()
+        self.datamodule = datamodule
+
+    def on_validation_end(self, trainer, pl_module):
+        val_bacth = next(iter(self.datamodule.val_dataloader()))
+        sentences = val_bacth['sentence']
+        labels = val_bacth['label']
+        outputs = pl_module(val_bacth['input_ids'], val_bacth['attention_mask'])
+        pred = torch.argmax(outputs.logits, dim=1)
+
+        df = pd.DataFrame({
+            'sentence': sentences,
+            'label': labels.cpu().numpy(),
+            'pred': pred.cpu().numpy()
+        })
+        wrong_predictions_df = df[df['label'] != df['pred']]
+        trainer.logger.experiment.log(
+            {
+                "wrong_predictions": wandb.Table(dataframe=wrong_predictions_df, allow_mixed_types=True),
+                "global_step": trainer.global_step
+            }
+        )
 
 def main():
     checkpoint_callback = ModelCheckpoint(
-    dirpath="./models",
-    monitor="val_acc",
-    mode="min",
+        dirpath="./models",
+        monitor="val_acc",
+        mode="min",
     )
     early_stopping_callback = EarlyStopping(
         monitor="val_acc",
@@ -23,13 +48,15 @@ def main():
         verbose=True,
         mode="min",
     )
+    wandb_logger = WandbLogger(project="cola_model", name="cola_run_1")
     trainer = pl.Trainer(
         default_root_dir = 'logs',
         gpus = (1 if torch.cuda.is_available() else 0),
-        max_epochs = 3,
+        max_epochs = 1,
+        log_every_n_steps = 10,
         fast_dev_run = True,
         logger = wandb_logger,
-        callbacks = [checkpoint_callback, early_stopping_callback]
+        callbacks = [checkpoint_callback, SimpleVisualizationLogger(cola_data), early_stopping_callback]
     )
     trainer.fit(cola_model, cola_data)
 
